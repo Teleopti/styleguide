34d88083e3d5f4eee775a50d2639dca7a09f71ea
blandine <blandine.descamps@gmail.com>
2015-04-13 15:45:16 +0200

    wip - calendar with range and icons update

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 var app = angular.module('styleguideApp', ['ngMaterial', 'ui.grid', 'ui.grid.autoResize', 'ui.grid.exporter', 'ui.grid.selection', 'ui.bootstrap'])
-.controller('mainCtrl',function($scope){
-			var data = [];
-			for(var i=0; i<100;i++){
-				data[i]={}
-				for(var j=0;j<10;j++){
-					data[i]["j"+j]=j;
-				}
-			}
-			$scope.gridOptions = {
-				exporterCsvFilename: 'myFile.csv',
-				exporterMenuPdf: false,
-				enableSelectAll: true,
-				enableRowSelection: true,
-				selectionRowHeaderWidth: 35,
-				data:data
-			};
-			$scope.gridOptions.enableGridMenu = true;
-		});
+.controller('mainCtrl', function ($scope) {
+  /*
+  * Code for Grid
+  */
+  var data = [];
+  for(var i=0; i<100;i++){
+    data[i]={}
+    for(var j=0;j<10;j++){
+	    data[i]["j"+j]=j;
+    }
+  }
+  $scope.gridOptions = {
+    exporterCsvFilename: 'myFile.csv',
+    exporterMenuPdf: false,
+    enableSelectAll: true,
+    enableRowSelection: true,
+    selectionRowHeaderWidth: 35,
+    data:data
+  };
+  $scope.gridOptions.enableGridMenu = true;
+
+  /*
+  * Code for datepicker
+  */
+  $scope.dayInRange = function(date, mode) {
+    console.log('inRange');
+  };
+
+});
