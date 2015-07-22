fa5c2cced489a2c4aaf389a8201f99961a6a4485
Robin Karlsson <robin.karlsson@teleopti.com>
2015-07-22 21:44:04 +0200

    Syntax again

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -10,7 +10,7 @@
  cp -R ../css ./css
  cp ../app.js ./app.js
  
- if [! git diff-index --quiet HEAD --]; then
+ if ! git diff-index --quiet HEAD --; then
  	git add .
  	git commit -m "Deployed to Github Pages"
  	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
