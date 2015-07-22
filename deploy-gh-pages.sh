6b9c833caa291ca38484dab9653ddf982360a505
Robin Karlsson <robin.karlsson@teleopti.com>
2015-07-22 18:00:36 +0200

    Trying out auto deploy to gh-pages

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm -rf out || exit 0;
+mkdir out; 
+( cd out
+ git init
+ git config user.name "Travis-CI"
+ git config user.email "carl.rockman@teleopti.com"
+ cp ../styleguide ./styleguide
+ git add .
+ git commit -m "Deployed to Github Pages"
+ git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
