e861342b9df28c9099c1657476f416eda077b19c
Robin Karlsson <robin.karlsson@teleopti.com>
2015-07-22 21:25:24 +0200

    Trying to keep history

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,12 +1,15 @@
-mkdir out; 
-( cd out
- git init
+( git clone -b gh-pages --single-branch https://github.com/Teleopti/styleguide.git out
+ cd out
+ 
+ find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
+ 
  git config user.name "Travis-CI"
  git config user.email "carl.rockman@teleopti.com"
  cp -R ../styleguide ./styleguide
  cp -R ../vendor ./vendor
  cp -R ../css ./css
  cp ../app.js ./app.js
+ 
  git add .
  git commit -m "Deployed to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
