fbd0924ed345475d538b28505eff4988cb604fe1
Robin Karlsson <robin.karlsson@teleopti.com>
2015-07-22 18:36:14 +0200

    Close now

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -3,7 +3,7 @@ mkdir out;
  git init
  git config user.name "Travis-CI"
  git config user.email "carl.rockman@teleopti.com"
- cp ../styleguide ./styleguide
+ cp -R ../styleguide ./styleguide
  git add .
  git commit -m "Deployed to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
