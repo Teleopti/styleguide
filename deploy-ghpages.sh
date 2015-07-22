f0ba140a399476f2577f6f37a39676b141d13c22
Robin Karlsson <robin.karlsson@teleopti.com>
2015-07-22 21:42:29 +0200

    Syntax issues

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -10,10 +10,9 @@
  cp -R ../css ./css
  cp ../app.js ./app.js
  
- if ! git diff-index --quiet HEAD --; then (
- git add .
- git commit -m "Deployed to Github Pages"
- git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
- )
+ if [! git diff-index --quiet HEAD --]; then
+ 	git add .
+ 	git commit -m "Deployed to Github Pages"
+ 	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
  fi
 )
