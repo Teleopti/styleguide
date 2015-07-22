5a22577f215d5b5cbf1122a21520a72618f1cd38
Robin Karlsson <robin.karlsson@teleopti.com>
2015-07-22 18:45:06 +0200

    Some more things need copy

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -5,6 +5,8 @@ mkdir out;
  git config user.email "carl.rockman@teleopti.com"
  cp -R ../styleguide ./styleguide
  cp -R ../vendor ./vendor
+ cp -R ../css ./styleguide/css
+ cp ../app.js ./app.js
  git add .
  git commit -m "Deployed to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
