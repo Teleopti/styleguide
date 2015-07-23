4930c65abb60d78bb1acc322161056cfb917e06a
Robin Karlsson <robin.karlsson@teleopti.com>
2015-07-23 10:07:34 +0200

    Addig auto deployment to Release archive

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -16,5 +16,22 @@
     # Uncommitted changes
  	git commit -m "Deployed to Github Pages"
  	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages > /dev/null 2>&1
+	
+	# Create version package and publish to Release
+	cp ../.npmignore ./.npmignore
+	cp ../package.json ./package.json
+	
+	npm pack
+	
+	cd ..
+	git clone -b Release --single-branch https://github.com/Teleopti/styleguide.git Release
+    cp -n out/*.tgz Release
+	
+	cd Release
+	if test -n "$(git status --porcelain)"; then
+    	# Uncommitted changes
+ 		git commit -m "Deployed to Release archive"
+ 		git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" Release > /dev/null 2>&1
+	fi
  fi
 )
