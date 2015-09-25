f80aea8d37562fdc8086743552e09ab3f66b5999
stephenwan <stephenwyy@gmail.com>
2015-09-25 22:35:45 +0800

    Added the documentation with minor issue fixes.

--- js/time-range-picker/time-range-picker.directive.js
+++ js/time-range-picker/time-range-picker.directive.js
@@ -5,6 +5,100 @@
     angular.module('wfm.timerangepicker', [])
            .directive('timepickerWrap', [timepickerWrap])
            .directive('timeRangePicker', ['$filter', timeRangePicker]);
+   
+
+    function timeRangePicker($filter) {
+        return {
+            template: getHtmlTemplate(),
+            scope: {
+                startTime: '=',
+                endTime: '=',
+                errors: '=?',
+                hideMessage: '=?'
+            },
+            controller: ['$scope', '$element', timeRangePickerCtrl],
+            require: ['timeRangePicker'],
+            link: postlink
+        };
+
+        function timeRangePickerCtrl($scope, $element) {
+            
+            $element.addClass('wfm-time-range-picker-wrap');
+
+            var errors = [];
+
+            if (angular.isDefined($scope.errors)) $scope.errors = errors;            
+
+            if (!$scope.startTime) $scope.startTime = moment({ hour: 8}).toDate();
+            if (!$scope.endTime) $scope.endTime = moment({hour: 17}).toDate();
+            
+            $scope.nextDay = isOnDifferentDays();
+            $scope.isInvalid = isInvalid;
+            
+            this.setDateValue = setDateValue;
+            this.checkValidity = checkValidity;
+            
+            function setDateValue(nextDay) {               
+                if (!nextDay) {                 
+                    var thisMomentDate = moment($scope.startTime);
+                    var endTimeMoment = moment($scope.endTime);
+                    thisMomentDate.set('hour', endTimeMoment.get('hour'))
+                    .set('minute', endTimeMoment.get('minute'));                        
+                    $scope.endTime = thisMomentDate.toDate();                    
+                } else {                  
+                    var nextMomentDate = moment($scope.startTime).add(1, 'day');
+                    var endTimeMoment = moment($scope.endTime);
+                    nextMomentDate.set('hour', endTimeMoment.get('hour'))
+                    .set('minute', endTimeMoment.get('minute'));                    
+                    $scope.endTime = nextMomentDate.toDate();                    
+                }
+            }
+
+            function isOnDifferentDays() {
+                return moment($scope.endTime).diff(moment($scope.startTime), 'days') > 0;
+            }
+
+            function isInvalid(symbol) {
+                if (symbol) {
+                    return errors.indexOf(symbol) >= 0;
+                } else {
+                    return errors.length > 0;
+                }   
+            }
+            
+            function checkValidity() {
+                errors.splice(0, errors.length);
+     
+                if (!$scope.startTime || !$scope.endTime) {
+		    errors.push('empty');
+		} else if ($scope.startTime >= $scope.endTime) {
+		    errors.push('order');
+		}    
+                if (angular.isDefined($scope.errors)) $scope.errors = errors;
+            }
+            
+        }
+
+        function postlink(scope, elem, attrs, ctrls) {
+            var ctrl = ctrls[0];
+                     
+            scope.$watch('nextDay', function(newValue, oldValue) {
+                ctrl.setDateValue(newValue);
+            });
+
+            scope.$watch(function() {
+                return {
+                    n: scope.nextDay,
+                    s: scope.startTime?$filter('date')(scope.startTime, 'HH:mm'): '',
+                    e: scope.endTime?$filter('date')(scope.endTime, 'HH:mm'): ''
+                }
+            }, function(newValue, oldValue) {                
+                ctrl.checkValidity();                
+            }, true);
+
+            scope.showMessage = !angular.isDefined(attrs.hideMessage); 
+        }
+    }
 
     function timepickerWrap() {
 
@@ -40,114 +134,13 @@
 	}
 
     }
-
-    function timeRangePicker($filter) {
-        return {
-            template: getHtmlTemplate(),
-            scope: {
-                startTime: '=',
-                endTime: '=',
-                disabled: '=?',
-                errors: '=?'
-            },
-            controller: ['$scope', '$element', timeRangePickerCtrl],
-            require: ['timeRangePicker'],
-            link: postlink
-        };
-
-        function timeRangePickerCtrl($scope, $element) {
-            
-            $element.addClass('wfm-time-range-picker-wrap');
-           
-
-            if (!$scope.startTime) $scope.startTime = moment({ hour: 8}).toDate();
-            if (!$scope.endTime) $scope.endTime = moment({hour: 17}).toDate();
-            
-            $scope.nextDay = isOnDifferentDays();
-            $scope.isInvalid = isInvalid;
-            
-            this.setDateValue = setDateValue;
-            this.checkValidity = checkValidity;
-            
-            function setDateValue(nextDay) {               
-                if (!nextDay) {
-                    if ( isOnDifferentDays()) {
-                        $scope.endTime.setFullYear($scope.startTime.getFullYear(),
-                                                   $scope.startTime.getMonth(),
-                                                   $scope.startTime.getDate());
-                    }
-                } else {
-                    if (! isOnDifferentDays()) {
-                        var nextMomentDate = moment($scope.startTime).add(1, 'day');
-                        $scope.endTime.setFullYear(nextMomentDate.get('year'),
-                                                   nextMomentDate.get('month'),
-                                                   nextMomentDate.get('date'));
-                    }
-                }
-            }
-
-            function isOnDifferentDays() {
-                return moment($scope.endTime).diff(moment($scope.startTime), 'days') > 0;
-            }
-
-            function isInvalid(symbol) {
-                if (!$scope.errors) $scope.errors = [];                
-                if (symbol) {
-                    return $scope.errors.indexOf(symbol) >= 0;
-                } else {
-                    return $scope.errors.length > 0;
-                }   
-            }
-            
-            function checkValidity() {
-                var errors = [];
-                if (!$scope.errors) $scope.errors = [];                
-     
-                if (!$scope.startTime || !$scope.endTime) {
-		    errors.push('empty');
-		} else if ($scope.startTime >= $scope.endTime) {
-		    errors.push('order');
-		}    
-                
-                if (errors.length != $scope.errors.length || !(function compareArray(a1, a2 ){
-                    for (var i = 0; i < a1.length; i++) {
-                        if (a1[i] !== a2[i]) return false;
-                    }
-                    return true;
-                } )(errors, $scope.errors)) {
-                    $scope.errors = errors;
-                }
-            }
-            
-        }
-
-        function postlink(scope, elem, attrs, ctrls) {
-            var ctrl = ctrls[0];
-                     
-            scope.$watch('nextDay', function(newValue, oldValue) {
-                ctrl.setDateValue(newValue);
-            });
-
-            scope.$watch(function() {
-                return {
-                    d: moment(scope.startTime).diff(moment(scope.endTime), 'days'),
-                    s: scope.startTime?$filter('date')(scope.startTime, 'HH:mm'): '',
-                    e: scope.endTime?$filter('date')(scope.endTime, 'HH:mm'): ''
-                }
-            }, function(newValue, oldValue) {                
-                ctrl.checkValidity();                
-            }, true);
-
-        }
-
-    }
-
+    
     function getHtmlTemplate() {
         return "<div ng-class=\"{'ng-valid': !isInvalid(), 'ng-invalid': isInvalid(), 'ng-invalid-order': isInvalid('order'), 'ng-invalid-empty': isInvalid('empty')}\">" +
           " <table> " +
           "   <tr> " +
           "     <td><timepicker-wrap ng-model=\"startTime\"></timepicker></td> " +
-          "     <td> <i class=\"mdi mdi-chevron-right\"> </i> </td> " +
+          "     <td> <i class=\"mdi mdi-minus\"> </i> </td> " +
           "     <td><timepicker-wrap ng-model=\"endTime\"></timepicker></td> " +
           "     <td> " +
           "       <div class=\"wfm-switch\"> " +
@@ -161,8 +154,8 @@
           "   </tr> " +
           " </table> " +
           "</div>" +
-          "<div class=\"error-msg-container ng-invalid-order alert-error notice-spacer m-t-10\"><i class='mdi mdi-alert-octagon'></i> <span translate>EndTimeMustBeGreaterOrEqualToStartTime</span></div> " +
-	  "<div class=\"error-msg-container ng-invalid-empty alert-error notice-spacer m-t-10\"><i class='mdi mdi-alert-octagon'></i> <span translate>StartTimeAndEndTimeMustBeSet</span></div>";
+          "<div class=\"error-msg-container ng-invalid-order alert-error notice-spacer\" ng-if=\"showMessage\"><i class='mdi mdi-alert-octagon'></i> <span translate>EndTimeMustBeGreaterOrEqualToStartTime</span></div> " +
+	  "<div class=\"error-msg-container ng-invalid-empty alert-error notice-spacer\" ng-if=\"showMessage\"><i class='mdi mdi-alert-octagon'></i> <span translate>StartTimeAndEndTimeMustBeSet</span></div>";
 
     }
 
