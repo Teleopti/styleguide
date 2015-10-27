210e9f5ff560cb838788c21cd0cd138c56022f57
Gan Hongzhao <Quentin.Kam@icloud.com>
2015-10-27 16:18:56 +0800

    Style guide 1.17 Pagination

--- app.js
+++ app.js
@@ -65,5 +65,55 @@ var chart = c3.generate({
 /*code for card list*/
 	$scope.items = [{ title: 'mdi-chart-bar' }, { title: 'mdi-chart-bar' }];
 
+/*code for pagination*/
+  $scope.pageCount = 7;
+  $scope.currentPage = 3;
+  $scope.firstPage = function(){
+    $scope.seekPage(1);
+  }
+  $scope.previousPage = function(){
+    $scope.seekPage($scope.currentPage - 1);
+  }
+  $scope.seekPage = function(page){
+    $scope.currentPage = page;
+  }
+  $scope.nextPage = function(){
+    $scope.seekPage($scope.currentPage + 1);
+  }
+  $scope.lastPage = function(){
+    $scope.seekPage($scope.pageCount);
+  }
+  
+  $scope.getVisiblePages = function (start, end) {
+		var displayPageCount = 5;
+		var ret = [];
+		if (!end) {
+			end = start;
+			start = 1;
+		}
+
+		var leftBoundary = start;
+		var rightBoundary = end;
+		if (end - start >= displayPageCount) {
+			var currentPage = $scope.currentPage;
+
+			if (currentPage < displayPageCount - 1) {
+				leftBoundary = 1;
+				rightBoundary = displayPageCount;
+			} else if (end - currentPage < 3) {
+				leftBoundary = end - displayPageCount + 1;
+				rightBoundary = end;
+			} else {
+				leftBoundary = currentPage - Math.floor(displayPageCount / 2) > 1 ? currentPage - Math.floor(displayPageCount / 2) : 1;
+				rightBoundary = currentPage + Math.floor(displayPageCount / 2) > end ? end : currentPage + Math.floor(displayPageCount / 2);
+			}
+		}
+
+		for (var i = leftBoundary; i <= rightBoundary ; i++) {
+			ret.push(i);
+		}
+
+		return ret;
+	};
 
 });
