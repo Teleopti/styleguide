7c7a21d38e9c26eabc5d05de9215a5f4b961eff6
johan damm <johan.b.damm@gmail.com>
2016-01-11 16:46:24 +0100

    Deploy on tagged releases
    Changed from version-number bound releases to tagged

--- deploy-package.sh
+++
@@ -1,8 +0,0 @@
-LOCAL=$(node -e "console.log(require('./package.json').version);");
-REMOTE=$(npm view teleopti-styleguide version);
-
-if [[ $LOCAL != $REMOTE ]];
-then
-	bash ./deploy-ghpages.sh;
-	return true
-fi
