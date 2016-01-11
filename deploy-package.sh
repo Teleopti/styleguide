38ded12608a5f50901e025ebd70d573113aa1778
johan damm <johan.b.damm@gmail.com>
2016-01-11 15:50:27 +0100

    attemted fix, change env if  is rejected

--- deploy-package.sh
+++ deploy-package.sh
@@ -3,9 +3,6 @@ REMOTE=$(npm view teleopti-styleguide version);
 
 if [[ $LOCAL != $REMOTE ]];
 then
-	npm config set username "teleopti_travis"
-	npm config set password $password
-	npm config set email $email
 	bash ./deploy-ghpages.sh;
-	npm publish;
+	return true
 fi
