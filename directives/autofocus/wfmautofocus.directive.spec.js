7fe131350a77df134c1629cb6910cd2eeddf3861
TheRockman <carl.rockman@teleopti.com>
2016-05-16 10:17:47 +0200

    autofocus test

---
+++ directives/autofocus/wfmautofocus.directive.spec.js
@@ -0,0 +1,25 @@
+
+describe('autofocus', function() {
+  var element;
+  beforeEach(
+    function() {
+      module('wfm.autofocus');
+    }
+  );
+
+  beforeEach(inject(function($compile, $rootScope) {
+      rootScope = $rootScope.$new();
+  }));
+
+  it('should set the focus on input', function () {
+    inject(function($compile, $timeout) {
+        var element = angular.element('<input type="text" autofocus/>');
+        elementCompile = $compile(element)(rootScope);
+        rootScope.$digest();
+
+        spyOn(element[0],'focus');
+        $timeout.flush();
+        expect(element[0].focus).toHaveBeenCalled();
+    });
+  })
+});
