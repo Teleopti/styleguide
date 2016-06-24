198490f095ec7df6543ac1b23b7e1fe2652dabe1
shenba2014 <luojunwenbashen@163.com>
2016-06-24 16:22:39 +0800

    Add test for go to page
    Add test for go to page

--- directives/pagination/wfmpagination.directive.spec.js
+++ directives/pagination/wfmpagination.directive.spec.js
@@ -3,7 +3,8 @@
     describe('pagination directive', function() {
       var elementCompile,
           scope,
-          directiveScope;
+          directiveScope,
+          targetScope;
 
       beforeEach(
         function() {
@@ -19,6 +20,7 @@
           elementCompile = $compile(element)(scope);
           scope.$digest();
           directiveScope = elementCompile.children().isolateScope();
+          targetScope = angular.element(elementCompile[0]).scope().$$childTail.vm;
       }));
 
       it('should display five pages number', function() {
@@ -26,6 +28,28 @@
           scope.$digest();
           expect(elementCompile[0].querySelectorAll('.pagination .pagination-item').length).toBe(9);
           expect(elementCompile[0].querySelectorAll('.pagination .pagination-item.disabled').length).toBe(2);
+      });
+
+      it('should trigger go to page', function() {
+          scope.paginationOptions = {totalPages: 3, pageNumber: 1};
+          scope.getPageData = function () {
+              isTriggered = true;
+          };
+          var isTriggered = false;
+          scope.$digest();
+          targetScope.gotoPage(2);
+          expect(isTriggered).toBe(true);
+      });
+
+      it('should not trigger go to page', function() {
+          scope.paginationOptions = {totalPages: 1, pageNumber: 1};
+          scope.getPageData = function () {
+              isTriggered = true;
+          };
+          var isTriggered = false;
+          scope.$digest();
+          targetScope.gotoPage(1);
+          expect(isTriggered).toBe(false);
       });
 
   });
