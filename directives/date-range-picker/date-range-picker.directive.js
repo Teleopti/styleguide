109d6f6e26aac6ea817a3da8518dbb11afed51d2
stephewan <stephenwyy@gmail.com>
2016-08-31 18:00:06 +0800

    Fix date-range-picker to work with ui-bootstrap2.x

--- directives/date-range-picker/date-range-picker.directive.js
+++ directives/date-range-picker/date-range-picker.directive.js
@@ -29,7 +29,6 @@
             var ngModelCtrl = ctrls[0],
                 dateRangeCtrl = ctrls[1];
 
-            scope.dummyMinDate = new Date('1970-01-01');
             scope.displayPopup = function() {
                 return scope.templateType === 'popup';
             };
@@ -39,8 +38,12 @@
 
             scope.dateFormat = $locale.DATETIME_FORMATS.shortDate;
 
-            scope.setRangeClass = setRangeClass;
-
+	    scope.datepickerOptions = {
+		customClass: getDayClass,
+		showWeeks: true,
+		startingDay: $locale.DATETIME_FORMATS.FIRSTDAYOFWEEK
+	    };
+	    
             scope.defaultValidators = [
               {
                   key: 'order',
@@ -154,10 +157,13 @@
 
             function refreshDatepickers() {
                 if (!scope.startDate || !scope.endDate) {return;}
-                scope.dummyMinDate = new Date('1970-01-01');
+		scope.datepickerOptions = angular.copy(scope.datepickerOptions);
             }
 
-            function setRangeClass(date, mode) {
+            function getDayClass(data) {
+		var date = data.date,
+		    mode = data.mode;
+				
                 if (ngModelCtrl.$invalid) {
                     return '';
                 }
