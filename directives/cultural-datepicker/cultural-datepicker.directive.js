0015550f5c8e81a731970007ba1ff1896bf1c6ba
Alina Morariu <alinan.morariu@yahoo.com>
2016-09-19 16:34:04 +0200

    refactored a little

--- directives/cultural-datepicker/cultural-datepicker.directive.js
+++ directives/cultural-datepicker/cultural-datepicker.directive.js
@@ -1,19 +1,21 @@
 (function() {
-'use strict';
-angular.module('wfm.culturalDatepicker', ['currentUserInfoService'])
-.controller('CulturalDatepickerCtrl', ['$scope', 'CurrentUserInfo', function($scope, CurrentUserInfo) {
-    var dateFormat = CurrentUserInfo.CurrentUserInfo().DateFormatLocale;
-    $scope.isJalaali = dateFormat === 'fa-IR' ? true : false;
-    $scope.isGregorian = !$scope.isJalaali;
-}])
-.directive('wfmCulturalDatepicker', function() {
-    return {
-        controller: 'CulturalDatepickerCtrl',
-        restrict: 'E',
-        scope: {
-            date: '=ngModel'
-        },
-        template: '<div class="wfm-datepicker-wrap wfm-cultural-datepicker-container"><persian-datepicker ng-if="isJalaali" ng-model="date" class="wfm-datepicker"></persian-datepicker><div uib-datepicker ng-if="isGregorian" ng-model="date" class="wfm-datepicker"></div></div>'
-    };
-});
+    'use strict';
+    angular.module('wfm.culturalDatepicker', ['currentUserInfoService'])
+        .controller('CulturalDatepickerCtrl', ['$scope', 'CurrentUserInfo', function($scope, CurrentUserInfo) {
+            var dateFormat = CurrentUserInfo.CurrentUserInfo().DateFormatLocale;
+            $scope.isJalaali = dateFormat === 'fa-IR' ? true : false;
+            $scope.isGregorian = !$scope.isJalaali;
+        }])
+        .directive('wfmCulturalDatepicker', function() {
+            var jalaaliTemplate = '<persian-datepicker ng-if="isJalaali" ng-model="date" class="wfm-datepicker"></persian-datepicker>';
+            var gregorianTemplate = '<div uib-datepicker ng-if="isGregorian" ng-model="date" class="wfm-datepicker"></div>';
+            return {
+                controller: 'CulturalDatepickerCtrl',
+                restrict: 'E',
+                scope: {
+                    date: '=ngModel'
+                },
+                template: '<div class="wfm-datepicker-wrap wfm-cultural-datepicker-container">' + jalaaliTemplate + gregorianTemplate + '</div>'
+            };
+        });
 })();
