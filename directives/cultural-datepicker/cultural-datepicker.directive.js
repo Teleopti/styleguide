5b3c39f7251b66b6f1db7ffbfbf3a7e2e0889613
Lin Sang Modé <lin.sang.mode@teleopti.com>
2017-03-02 06:24:46 +0100

    fixed merge issues

--- directives/cultural-datepicker/cultural-datepicker.directive.js
+++ directives/cultural-datepicker/cultural-datepicker.directive.js
@@ -1,5 +1,4 @@
 (function() {
-<<<<<<< HEAD
     'use strict';
     angular.module('wfm.culturalDatepicker', ['currentUserInfoService'])
         .controller('CulturalDatepickerCtrl', ['$scope', 'CurrentUserInfo', function($scope, CurrentUserInfo) {
@@ -20,27 +19,4 @@
                 template: '<div class="wfm-datepicker-wrap wfm-cultural-datepicker-container">' + jalaaliTemplate + gregorianTemplate + '</div>'
             };
         });
-=======
-'use strict';
-angular.module('wfm.culturalDatepicker', ['currentUserInfoService'])
-.controller('CulturalDatepickerCtrl', ['$scope', 'CurrentUserInfo', function($scope, CurrentUserInfo) {
-    var dateFormat = CurrentUserInfo.CurrentUserInfo().DateFormatLocale;
-    $scope.isJalaali = dateFormat === 'fa-IR' ? true : false;
-    $scope.isGregorian = !$scope.isJalaali;
-}])
-.directive('wfmCulturalDatepicker', ['CurrentUserInfo',function(CurrentUserInfo) {
-    return {
-        controller: 'CulturalDatepickerCtrl',
-        restrict: 'E',
-        scope: {
-            date: '=ngModel'
-        },
-        template:
-        '<div class="wfm-datepicker-wrap wfm-cultural-datepicker-container">' +
-        '<persian-datepicker ng-if="isJalaali" ng-model="date" class="wfm-datepicker"></persian-datepicker>' +
-        '<div uib-datepicker ng-if="isGregorian" ng-model="date" class="wfm-datepicker"></div>' +
-        '</div>'
-    };
-}]);
->>>>>>> 564f537f3e9334297b9ae3123b78b269f0dceece
 })();
