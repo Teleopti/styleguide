83085e373248df335924effd3fabb5fa55a33157
Jianfeng <jeff.doyle@foxmail.com>
2017-06-13 14:35:21 +0800

    Add missing dependency resizable css and show resizer of rightpanel

--- js/app.js
+++ js/app.js
@@ -187,7 +187,7 @@
           panelState: false,
           sidePanelTitle: 'Sidepanel title',
           showCloseButton: true,
-          showResizer: false,
+          showResizer: true,
           showBackdrop: true,
           showPopupButton: true
       };
