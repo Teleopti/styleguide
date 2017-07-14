554b5bfb8a50d50aeaa1ae43c59e955e71a84966
Qing Luo <Qing.Luo@teleopti.com>
2017-07-14 14:45:45 +0800

    Add keyboard control for multiple search input

--- directives/wfm-multiple-search/wfm-multiple-search-input.directive.js
+++ directives/wfm-multiple-search/wfm-multiple-search-input.directive.js
@@ -19,7 +19,7 @@
         };
 
         vm.toggleAdvancedSearchOption = function ($event) {
-            vm.showAdvancedSearchOption = !vm.showAdvancedSearchOption;
+            vm.showAdvancedSearchOption = true;
             $event.stopPropagation();
             parseSearchExpressionInputted();
         };
@@ -42,13 +42,35 @@
             }
         };
 
-        vm.focusSearch = function() {
+        vm.handleAdvanceSearchShowup = function () {
+            if (!vm.searchOptions.keyword) {
+                vm.showAdvancedSearchOption = true;
+            } else {
+                vm.showAdvancedSearchOption = false;
+            }
+        };
+
+        vm.focusSearch = function () {
             vm.searchOptions.focusingSearch = true;
         };
 
         vm.resetFocusSearch = function () {
             vm.searchOptions.focusingSearch = false;
             return true;
+        };
+
+        vm.searchTextChange = function () {
+            vm.validateSearchKeywordChanged();
+            vm.handleAdvanceSearchShowup();
+        };
+
+        vm.searchTextInputKeydown = function (event) {
+            if (event.which === 13) {
+                vm.resetFocusSearch();
+                vm.searchCallback(vm.searchOptions.keyword);
+                vm.turnOffAdvancedSearch();
+                return;
+            }
         };
 
         function setSearchFormProperty(searchType, searchValue) {
@@ -77,8 +99,8 @@
             quotedKeywords = quotedKeywords.trim();
 
             var unquotedKeywords = displayValue
-        .replace(pattern, '').trim()
-        .replace('"', '').trim();
+                .replace(pattern, '').trim()
+                .replace('"', '').trim();
 
             return (quotedKeywords + ' ' + unquotedKeywords).trim();
         }
@@ -121,7 +143,7 @@
     };
 
     angular.module('wfm.multiplesearchinput', ['wfm.helpingDirectives'])
-     .directive('wfmMultipleSearchInput', multipleSearchInputDirective)
-     .controller('multipleSearchInputCtrl', multipleSearchInputCtrl);
+        .directive('wfmMultipleSearchInput', multipleSearchInputDirective)
+        .controller('multipleSearchInputCtrl', multipleSearchInputCtrl);
 
 })();
